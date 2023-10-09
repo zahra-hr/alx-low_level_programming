d9cb6297591c8e5fcafa7a38e5c4555701f5b738
zahra <zahraezzahra1522@gmail.com>
2023-10-09 10:40:48 -0700

    if-else

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: combination of single digits
+ * Return: 0 if success
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i == 9)
+			putchar('0' + i);
+		else
+		{
+			putchar('0' + i);
+			putchar(',');
+			putchar(' ');
+			i++;
+		}
+	}
+	return (0);
+}
