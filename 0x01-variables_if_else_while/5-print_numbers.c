b701d8e3df365df17fa7e75356cc2358945f1a59
zahra <zahraezzahra1522@gmail.com>
2023-10-09 10:05:40 -0700

    if-else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program to print digits
+ * Return: 0 if success
+*/
+int main(void)
+{
+	char i = 48;
+
+	for (; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
