5d6fdce6c7a3506821f801243fa613d0e253d703
zahra <zahraezzahra1522@gmail.com>
2023-10-10 13:08:04 -0700

    headers

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: print _putchar
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	_putchar(95);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
