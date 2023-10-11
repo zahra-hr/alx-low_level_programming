65ebf1e8958a9a4037ca27f4a06fc5eb5a7a0a88
zahra <zahraezzahra1522@gmail.com>
2023-10-11 01:03:13 -0700

    headers

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: function that prints the alphabet
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
