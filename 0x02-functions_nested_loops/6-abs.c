8a289a4a0e06de3f2b51467867858614c5df1aa8
zahra <zahraezzahra1522@gmail.com>
2023-10-11 13:58:29 -0700

    headers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - function to find absolute number
+ * @c: parameter
+ * Return: integer
+*/
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-1 * c);
+}
