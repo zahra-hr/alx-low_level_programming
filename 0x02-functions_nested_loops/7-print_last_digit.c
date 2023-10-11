e871bdff60afd213a894ac7b4166c086ac00edfe
zahra <zahraezzahra1522@gmail.com>
2023-10-11 14:03:56 -0700

    headers

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - function to find last digit
+ * @d: parameter
+ * Return: Integer
+*/
+
+int print_last_digit(int d)
+{
+	int j = d % 10;
+
+	return (j);
+}
